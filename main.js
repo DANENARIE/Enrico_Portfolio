9b7350977261f7e2f837db10fef4d4a04fa3c037
DANE NARIE <136543262+DANENARIE@users.noreply.github.com>
2023-12-26 09:27:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+//Dark Mode effect//
+let darkModeIcon = document.querySelector("#darkMode-icon");
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("night-theme");
+};
+
+//header scroll toggle//
+window.addEventListener("scroll", () => {
+  let header = document.querySelector(".header");
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+});
+
+//==SCROLL SECTION ACTIVE LINKS====//
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      document
+        .querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
+    }
+  });
+};
+
+// Menu icon bar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Scroll reveal
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ", .services-container, .portfolio-box, .testimonial-wrapper, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img img ", { origin: "left" });
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-content ", {
+  origin: "right",
+});
+
+function changeImage() {
+  var img = document.getElementById("dayimage");
+  img.src = "assets/nigtdayforest.png";
+}
